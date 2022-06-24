038c18392ea52af4ff36e0f43b902012a1c1a947
unkn0wnb4ain <108135638+unkn0wnb4ain@users.noreply.github.com>
2022-06-24 19:15:09 +0530

    my_cp_template

---
+++ cp_template.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+typedef pair<ll,ll> pll;
+typedef vector <ll> vll;
+
+#define nl  "\n"
+#define ft  first
+#define sc  second
+#define pub push_back
+#define pob pop_back()
+
+#define loop(i, a, b) for(ll i = a; i < b; ++i)
+#define pool(i, a, b) for(ll i = a; i >= b; --i)
+
+#define szz(x) (ll)x.size()
+#define alx(x) x.begin(), x.end()
+#define dbg(x) cout << #x << ":" << x << nl;
+
+#define inn(x) for(auto &i : x) cin >> i;
+#define out(x) for(auto &i : x) cout << i << " "; cout << nl; 
+
+#define xLR8() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+#define in2d(x, n, m, a) loop(i, x, n) {loop(j, x, m) cin >> a[i][j];}
+#define ou2d(x, n, m, a) loop(i, x, n) {loop(j, x, m) cout << a[i][j] << " "; cout << nl;}
+
+
+
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+void ____________________()
+{
+
+    
+
+} 
+
+
+
+
+int main()
+{
+    
+    xLR8()
+
+    int t; t = 1; cin >> t;
+    for(ll i = 1; i <= t; i++)
+    {
+        ____________________();
+    }
+    
+}
+
