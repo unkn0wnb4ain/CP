e976a3cab6da58b02d4d6621c4462b994fd6f47c
unkn0wnb4ain <108135638+unkn0wnb4ain@users.noreply.github.com>
2023-03-12 00:08:23 +0530

    Update bipartite_check.cpp

--- bipartite_check.cpp
+++ bipartite_check.cpp
@@ -1,25 +1,24 @@
+function <void(ll)> bfs = [&](ll node)
+{
+    queue <ll> q;
+    q.push(node);
 
-	function <void(ll)> bfs = [&](ll node)
-	{
-		queue <ll> q;
-		q.push(node);
+    col[node] = 0;
+    while(q.size())
+    {
+        ll node_ = q.front();
+        q.pop();
 
-		col[node] = 0;
-		while(q.size())
-		{
-			ll node_ = q.front();
-			q.pop();
+        for(auto child : graph[node_])
+        {
+            if(col[child] == -1)
+            {
+                col[child] = 1 ^ col[node_];
+                q.push(child);
+            }
 
-			for(auto child : graph[node_])
-			{
-				if(col[child] == -1)
-				{
-					col[child] = 1 ^ col[node_];
-					q.push(child);
-				}
-
-				else
-					bipartite &= (col[node_] != col[child]);
-			}
-		}
-	};
+            else
+                bipartite &= (col[node_] != col[child]);
+        }
+    }
+};
