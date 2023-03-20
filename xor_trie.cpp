c66efb302c9e421f175efbed1904055463dc2f0b
unkn0wnb4ain <108135638+unkn0wnb4ain@users.noreply.github.com>
2023-03-20 21:18:18 +0530

    Update xor_trie.cpp

--- xor_trie.cpp
+++ xor_trie.cpp
@@ -1,60 +1,78 @@
-struct node 
+template<int bits>
+class Trie 
 {
-    node* next[2];
-    int f[2]{};
-
-    node () 
+private:
+    template<int bt>
+    struct node 
     {
-        next[0] = nullptr;
-        next[1] = nullptr;
+        int cnt;
+        node <bt>* bit[2];
+        node(): cnt(0), bit{nullptr, nullptr} {}
+    };
+    node <bits>* root;
+ 
+public:
+    Trie(): root(new node <bits>()) {}
+ 
+    void insert(ll x) 
+    {
+        auto curr = root;
+        for (int i = bits - 1; i >= 0; --i) 
+        {
+            ll b = (x >> i) & 1;
+            if (!curr->bit[b])
+                curr->bit[b] = new node<bits>();
+ 
+            curr = curr->bit[b];
+            curr->cnt++;
+        }
+    }
+ 
+    void remove(ll x) 
+    {
+        auto curr = root;
+        for (int i = bits - 1; i >= 0; --i) 
+        {
+            ll b = (x >> i) & 1;
+            curr = curr->bit[b];
+            curr->cnt--;
+        }
+    }
+ 
+    ll min_xor(ll x) const 
+    {
+        ll res = 0;
+        auto curr = root;
+        for (int i = bits - 1; i >= 0; --i) 
+        {
+            ll b = (x >> i) & 1;
+            if (curr->bit[b] && curr->bit[b]->cnt > 0) 
+                curr = curr->bit[b]; 
+            else 
+            {
+                res |= 1ll << i;
+                b ^= 1;
+                curr = curr->bit[b];
+            }
+        }
+        return res;
+    }
+ 
+    ll max_xor(ll x) const 
+    {
+        ll res = 0;
+        auto curr = root;
+        for (int i = bits - 1; i >= 0; --i) 
+        {
+            int b = ((x >> i) & 1) ^ 1;
+            if (curr->bit[b] && curr->bit[b]->cnt > 0) 
+            {
+                res |= 1ll << i;
+                curr = curr->bit[b];
+            } 
+            else
+                curr = curr->bit[b ^ 1];
+        }
+        return res;
     }
 };
-
-void insert(node* &root, int num) 
-{
-    node* curr = root;
-    for (int bit = 30; bit >= 0; bit--)
-    {
-        int mask = (1 << bit);
-        int _bit = ((mask & num) > 0);
-        if (curr -> next[_bit] == nullptr)
-            curr -> next[_bit] = new node();
-        curr -> f[_bit]++;
-        curr = curr -> next[_bit];
-    }
-}
-
-void remove (node* &root, int num)
-{
-    node* curr = root;
-    for (int bit = 30; bit >= 0; bit--)
-    {
-        int mask = (1 << bit);
-        int _bit = ((mask & num) > 0);
-        curr -> f[_bit]--;
-        if (curr -> f[_bit] == 0)
-            curr -> next[_bit] = nullptr;
-        curr = curr -> next[_bit];
-        if (curr == nullptr)
-            break;
-    }
-}
-
-int find (node* &root, int num) 
-{
-    int ans = 0;
-    node* curr = root;
-    for (int bit = 30; bit >= 0; bit--) 
-    {
-        int mask = (1 << bit);
-        int _bit = ((mask & num) ? 1 : 0);
-        ans |= (curr -> next[_bit] == nullptr) ^ _bit ? mask : 0;
-        if (curr -> next[_bit] != nullptr) 
-            curr = curr -> next[_bit];
-        else 
-            curr = curr -> next[_bit ^ 1];
-        if (curr == nullptr)
-            break;
-    }
-    return ans;
-}
