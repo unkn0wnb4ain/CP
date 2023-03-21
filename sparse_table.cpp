88d20f2bced7632e3cf2ad6574345e3bdb1ccd6f
unkn0wnb4ain <108135638+unkn0wnb4ain@users.noreply.github.com>
2023-03-21 15:43:39 +0530

    Update sparse_table.cpp

--- sparse_table.cpp
+++ sparse_table.cpp
@@ -2,36 +2,37 @@ template <typename T>
 struct SparseTable
 {
     ll n, m;
-    vector <vector <T> > st;
+    vector <int> lg;
+    vector <vector <T>> st;
 
-    SparseTable(ll nn, ll mm)
+    SparseTable(vector <T> &a)
     {
-        n = nn; m = mm;
+        n = a.size();
+
+        lg.assign(n + 1, 0);
+        for (int i = 2; i <= n; ++i)
+            lg[i] = lg[i / 2] + 1;
+
+        m = lg[n] + 1;
         st.assign(n + 1, vector <T> (m, 0));
-    }
 
-    inline T Function(T &x, T &y)
-    {
-        return min(x, y);
-    }
-
-    void Build(ll a[], ll n, string op)
-    {
         for(ll i = 0; i < n; ++i)
             st[i][0] = a[i];
 
         for(ll k = 1; k < m; ++k)
             for(ll i = 0; i + (1 << k) - 1 < n; ++i)
-                st[i][k] = Function(st[i][k - 1], st[i + (1 << (k - 1))][k - 1]);
+                st[i][k] = func(st[i][k - 1], st[i + (1 << (k - 1))][k - 1]);
     }
 
-    T Query(ll l, ll r, string op)
+    T func(T &x, T &y)
     {
-        ll len = r - l + 1;
-        ll log = 0;
-        while((1 << (log + 1)) <= len)
-            log++;
+        return min(x, y);
+    }
 
-        return Function(st[l][log], st[r - (1 << log) + 1][log]);
+    T Query(ll l, ll r)
+    {
+        l--; r--;
+        ll log = lg[r - l + 1];
+        return func(st[l][log], st[r - (1 << log) + 1][log]);
     }
 };
